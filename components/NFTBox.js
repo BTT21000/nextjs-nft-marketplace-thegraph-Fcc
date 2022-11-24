9075e1178f6975d7630e60849f0aa3d44162951c
BTT21000 <34821063+BTT21000@users.noreply.github.com>
2022-11-24 23:23:56 +1100

    added next/image components

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import { useWeb3Contract, useMoralis } from "react-moralis"
 import nftMarketplaceAbi from "../constants/NftMarketplace.json"
 import nftAbi from "../constants/BasicNft.json"
+import Image from "next/image"
 import { Card, useNotification } from "web3uikit"
 import { ethers } from "ethers"
 import UpdateListingModal from "./UpdateListingModal"
@@ -120,7 +121,7 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
                                     <div className="italic text-sm">
                                         Owned by {formattedSellerAddress}
                                     </div>
-                                    <img
+                                    <Image
                                         loader={() => imageURI}
                                         src={imageURI}
                                         height="200"
