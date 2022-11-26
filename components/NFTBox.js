1fb463a39cdd18641cf2c07663e48f259c70893b
BTT21000 <34821063+BTT21000@users.noreply.github.com>
2022-11-26 19:31:59 +1100

    addition of antd package and code upate to fix UI images

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -6,6 +6,7 @@ import Image from "next/image"
 import { Card, useNotification } from "web3uikit"
 import { ethers } from "ethers"
 import UpdateListingModal from "./UpdateListingModal"
+import { Skeleton } from "antd"
 
 const truncateStr = (fullStr, strLen) => {
     if (fullStr.length <= strLen) return fullStr
@@ -27,6 +28,7 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
     const [tokenDescription, setTokenDescription] = useState("")
     const [showModal, setShowModal] = useState(false)
     const hideModal = () => setShowModal(false)
+    const [isURILoaded, setIsURILoaded] = useState(false)
     const dispatch = useNotification()
 
     const { runContractFunction: getTokenURI } = useWeb3Contract({
@@ -62,6 +64,7 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
             setImageURI(imageURIURL)
             setTokenName(tokenURIResponse.name)
             setTokenDescription(tokenURIResponse.description)
+            setIsURILoaded(true)
             // We could render the Image on our sever, and just call our sever.
             // For testnets & mainnet -> use moralis server hooks
             // Have the world adopt IPFS
@@ -121,12 +124,24 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
                                     <div className="italic text-sm">
                                         Owned by {formattedSellerAddress}
                                     </div>
-                                    <Image
-                                        loader={() => imageURI}
-                                        src={imageURI}
-                                        height="200"
-                                        width="200"
-                                    />
+                                    <div className="image">
+                                        {imageURI ? (
+                                            <Image
+                                                loader={() => imageURI}
+                                                src={imageURI}
+                                                height="200"
+                                                width="200"
+                                            />
+                                        ) : (
+                                            <Skeleton.Image
+                                                style={{ width: 300, height: 300 }}
+                                                loading={!isURILoaded}
+                                                active
+                                                className="nft-skeleton"
+                                            />
+                                        )}
+                                    </div>
+
                                     <div className="font-bold">
                                         {ethers.utils.formatUnits(price, "ether")} ETH
                                     </div>
@@ -141,3 +156,150 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
         </div>
     )
 }
+
+///// Former code prior to image fix with antd package for website deployment /////
+///// As explained here https://github.com/smartcontractkit/full-blockchain-solidity-course-js/discussions/3905
+
+// import { useState, useEffect } from "react"
+// import { useWeb3Contract, useMoralis } from "react-moralis"
+// import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+// import nftAbi from "../constants/BasicNft.json"
+// import Image from "next/image"
+// import { Card, useNotification } from "web3uikit"
+// import { ethers } from "ethers"
+// import UpdateListingModal from "./UpdateListingModal"
+
+// const truncateStr = (fullStr, strLen) => {
+//     if (fullStr.length <= strLen) return fullStr
+
+//     const separator = "..."
+//     const seperatorLength = separator.length
+//     const charsToShow = strLen - seperatorLength
+//     const frontChars = Math.ceil(charsToShow / 2)
+//     const backChars = Math.floor(charsToShow / 2)
+//     return (
+//         fullStr.substring(0, frontChars) + separator + fullStr.substring(fullStr.length - backChars)
+//     )
+// }
+
+// export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {
+//     const { isWeb3Enabled, account } = useMoralis()
+//     const [imageURI, setImageURI] = useState("")
+//     const [tokenName, setTokenName] = useState("")
+//     const [tokenDescription, setTokenDescription] = useState("")
+//     const [showModal, setShowModal] = useState(false)
+//     const hideModal = () => setShowModal(false)
+//     const dispatch = useNotification()
+
+//     const { runContractFunction: getTokenURI } = useWeb3Contract({
+//         abi: nftAbi,
+//         contractAddress: nftAddress,
+//         functionName: "tokenURI",
+//         params: {
+//             tokenId: tokenId,
+//         },
+//     })
+
+//     const { runContractFunction: buyItem } = useWeb3Contract({
+//         abi: nftMarketplaceAbi,
+//         contractAddress: marketplaceAddress,
+//         functionName: "buyItem",
+//         msgValue: price,
+//         params: {
+//             nftAddress: nftAddress,
+//             tokenId: tokenId,
+//         },
+//     })
+
+//     async function updateUI() {
+//         const tokenURI = await getTokenURI()
+//         console.log(`The TokenURI is ${tokenURI}`)
+//         // We are going to cheat a little here...
+//         if (tokenURI) {
+//             // IPFS Gateway: A server that will return IPFS files from a "normal" URL.
+//             const requestURL = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+//             const tokenURIResponse = await (await fetch(requestURL)).json()
+//             const imageURI = tokenURIResponse.image
+//             const imageURIURL = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+//             setImageURI(imageURIURL)
+//             setTokenName(tokenURIResponse.name)
+//             setTokenDescription(tokenURIResponse.description)
+//             // We could render the Image on our sever, and just call our sever.
+//             // For testnets & mainnet -> use moralis server hooks
+//             // Have the world adopt IPFS
+//             // Build our own IPFS gateway
+//         }
+//         // get the tokenURI
+//         // using the image tag from the tokenURI, get the image
+//     }
+
+//     useEffect(() => {
+//         if (isWeb3Enabled) {
+//             updateUI()
+//         }
+//     }, [isWeb3Enabled])
+
+//     const isOwnedByUser = seller === account || seller === undefined
+//     const formattedSellerAddress = isOwnedByUser ? "you" : truncateStr(seller || "", 15)
+
+//     const handleCardClick = () => {
+//         isOwnedByUser
+//             ? setShowModal(true)
+//             : buyItem({
+//                   onError: (error) => console.log(error),
+//                   onSuccess: () => handleBuyItemSuccess(),
+//               })
+//     }
+
+//     const handleBuyItemSuccess = () => {
+//         dispatch({
+//             type: "success",
+//             message: "Item bought!",
+//             title: "Item Bought",
+//             position: "topR",
+//         })
+//     }
+
+//     return (
+//         <div>
+//             <div>
+//                 {imageURI ? (
+//                     <div>
+//                         <UpdateListingModal
+//                             isVisible={showModal}
+//                             tokenId={tokenId}
+//                             marketplaceAddress={marketplaceAddress}
+//                             nftAddress={nftAddress}
+//                             onClose={hideModal}
+//                         />
+//                         <Card
+//                             title={tokenName}
+//                             description={tokenDescription}
+//                             onClick={handleCardClick}
+//                         >
+//                             <div className="p-2">
+//                                 <div className="flex flex-col items-end gap-2">
+//                                     <div>#{tokenId}</div>
+//                                     <div className="italic text-sm">
+//                                         Owned by {formattedSellerAddress}
+//                                     </div>
+//                                     <Image
+//                                         loader={() => imageURI}
+//                                         src={imageURI}
+//                                         height="200"
+//                                         width="200"
+//                                     />
+//                                     <div className="font-bold">
+//                                         {ethers.utils.formatUnits(price, "ether")} ETH
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </Card>
+//                     </div>
+//                 ) : (
+//                     <div>Loading...</div>
+//                 )}
+//             </div>
+//         </div>
+//     )
+// }
--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     swcMinify: true,
+//     // images: {
+//     //     unoptimized: true,
+//     // },
+//     images: {
+//         loader: "imgix",
+//         path: "/",
+//     },
+// }
+
 const nextConfig = {
-    reactStrictMode: true,
+    reactStrictMode: false,
     swcMinify: true,
-    // images: {
-    //     unoptimized: true,
-    // },
     images: {
-        loader: "imgix",
-        path: "/",
+        loader: "akamai",
+        path: "",
+        domains: ["cdn.jsdelivr.net"],
     },
 }
 
