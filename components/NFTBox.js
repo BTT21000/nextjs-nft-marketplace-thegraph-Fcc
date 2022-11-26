5ebde95a56257bd875c445bd928b4ffd292e1a72
BTT21000 <34821063+BTT21000@users.noreply.github.com>
2022-11-26 20:02:21 +1100

    Image package replaced by standard img

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -126,7 +126,7 @@ export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress,
                                     </div>
                                     <div className="image">
                                         {imageURI ? (
-                                            <Image
+                                            <img
                                                 loader={() => imageURI}
                                                 src={imageURI}
                                                 height="200"
